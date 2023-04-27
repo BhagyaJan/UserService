40816982d308ce96309fd5b1036b340e58e62087
BhagyaJan <packiaraj1501@gmail.com>
2023-04-27 15:21:58 +0200

    test commit

--- src/test/java/com/behind/graphs/UserServiceSapApplicationTests.java
+++ src/test/java/com/behind/graphs/UserServiceSapApplicationTests.java
@@ -1,13 +1,17 @@
 package com.behind.graphs;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class UserServiceSapApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true, true);
 	}
 
 }
