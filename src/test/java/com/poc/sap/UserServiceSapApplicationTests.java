4969056f0ca18eb83105806614ecf2506a180b69
BhagyaJan <packiaraj1501@gmail.com>
2023-04-27 15:43:38 +0200

    test commit3

--- src/test/java/com/poc/sap/UserServiceSapApplicationTests.java
+++ src/test/java/com/poc/sap/UserServiceSapApplicationTests.java
@@ -3,12 +3,10 @@ package com.poc.sap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = UserServiceSapApplicationTests.class)
+
+@SpringBootTest
 class UserServiceSapApplicationTests {
 
 	@Test
